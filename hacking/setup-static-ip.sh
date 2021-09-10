0f1d83607b72b9dd022e05303080627d6847ce1f
Malcolm Jones <bossjones@theblacktonystark.com>
2021-09-10 16:45:10 -0400

    wip: trying this out

--- hacking/setup-static-ip.sh
+++ hacking/setup-static-ip.sh
@@ -17,6 +17,13 @@ echo '    user_friendly_names yes' | sudo tee -a /etc/multipath.conf
 echo '} ' | sudo tee -a /etc/multipath.conf
 echo 'blacklist {' | sudo tee -a /etc/multipath.conf
 echo '    devnode "^(ram|raw|loop|fd|md|dm-|sr|scd|st|sd|mmc|nvme)[a-z0-9]*"' | sudo tee -a /etc/multipath.conf
+# SOURCE: https://sleeplessbeastie.eu/2021/01/06/how-to-fix-multipath-daemon-error-about-missing-path-when-using-virtualbox/
+echo '  device {' | sudo tee -a /etc/multipath.conf
+echo '    vendor "VMware,"' | sudo tee -a /etc/multipath.conf
+echo '    product "VMware Virtual S"' | sudo tee -a /etc/multipath.conf
+echo '  }' | sudo tee -a /etc/multipath.conf
+
+echo '    devnode "^(ram|raw|loop|fd|md|dm-|sr|scd|st|sd|mmc|nvme)[a-z0-9]*"' | sudo tee -a /etc/multipath.conf
 echo '}' | sudo tee -a /etc/multipath.conf
 
 echo "-------------------------------------------"
@@ -27,3 +34,4 @@ sudo cat /etc/multipath.conf
 
 echo "network: {config: disabled}" | tee /etc/cloud/cloud.cfg.d/subiquity-disable-cloudinit-networking.cfg
 sudo netplan apply
+sudo systemctl restart multipathd.service
